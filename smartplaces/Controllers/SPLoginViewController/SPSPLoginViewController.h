8965e2a25620c3cb641311bbd0f6ab6d394fff8c
Ihor Shevchuk <ihor.shevchuk@techmagic.co>
2015-12-09 00:05:16 +0200

    Moved files according template

---
+++ smartplaces/Controllers/SPLoginViewController/SPSPLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SPSPLoginViewController.h
+//  smartplaces
+//
+//  Created by Admin on 12/8/15.
+//  Copyright © 2015 ihor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SPSPLoginViewController : UIViewController
+
+@end
