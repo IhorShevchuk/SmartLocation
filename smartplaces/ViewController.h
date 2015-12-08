414da34f919f4a2a40e3d06a9eb0cab906677bd2
Ihor Shevchuk <ihor.shevchuk@techmagic.co>
2015-12-08 23:32:56 +0200

    Initial Commit

---
+++ smartplaces/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  smartplaces
+//
+//  Created by Admin on 12/8/15.
+//  Copyright © 2015 ihor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
